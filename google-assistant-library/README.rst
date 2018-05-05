ff5300a158731f95b7a4cc198734c817dcafcac7
Google Assistant SDK Team <jordanjtw+assistant-library@google.com>
2018-05-06 06:44:28 +0900

    google-assistant-library: sync sample changes
    - update README
    - device model caching
    - unify command line flag
    - fix python 2.7 printing
    - fixes device actions handler without params
    
    GitOrigin-RevId: 24254b4c46abdc5245d4d0da4f45765f67b6c641
    Change-Id: I64458cfd87f3ee5e7aa23540309df59a2a545a96

--- google-assistant-library/README.rst
+++ google-assistant-library/README.rst
@@ -3,7 +3,7 @@ Google Assistant Library for Python
 
 This package contains high level Python_ bindings for the `Google Assistant Library`_.
 
-It is part of the `Google Assistant SDK`_.
+It is part of the `Google Assistant SDK for devices`_.
 
 It includes the following features:
 
@@ -13,8 +13,10 @@ It includes the following features:
 - Timer and alarm features
 - Volume ducking and control
 - Conversation state management
+- Playback of podcasts and news
+- Broadcast voice messages
 
-See `Introduction to the Google Assistant Library`_ for a step by step guide on how to get started with the library on the Raspberry Pi 3.
+See `Introduction to the Google Assistant Library`_ for a step by step guide on how to get started with the library on Raspberry Pi 3.
 
 Supported configuration
 -----------------------
@@ -32,22 +34,26 @@ Installing
 Authorization
 -------------
 
-- Follow the steps to `configure the Actions Console project and the Google account <httpsb://developers.google.com/assistant/sdk/guides/library/python/embed/config-dev-project-and-account>`_.
+- Follow the steps to `configure the Actions Console project and the Google account <https://developers.google.com/assistant/sdk/guides/library/python/embed/config-dev-project-and-account>`_.
 - Follow the steps to `register a new device model and download the client secrets file <https://developers.google.com/assistant/sdk/guides/library/python/embed/register-device>`_.
-- Generate device credentials using ``google-oauthlib-tool``:
+- Generate credentials using ``google-oauth-tool``.::
 
     pip install --upgrade google-auth-oauthlib[tool]
-    google-oauthlib-tool --client-secrets path/to/credentials.json --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless
+    google-oauthlib-tool --client-secrets path/to/client_secret_XXXXX.json --scope https://www.googleapis.com/auth/assistant-sdk-prototype --scope https://www.googleapis.com/auth/gcm --save --headless
 
 Usage
 -----
 
 - Run the demo::
 
-    google-assistant-demo --device_model_id 'my-model-identifier'
+    google-assistant-demo
 
 - Say "Ok Google" or "Hey Google" followed by a voice query. The demo should
   play back the Assistant's response and log events to the screen.
+
+- Register the device and run the demo with device actions::
+
+    google-assistant-demo --project-id PROJECT_ID --device-model-id DEVICE_MODEL
 
 Reference
 ---------
@@ -62,11 +68,10 @@ See `LICENSE`_ and `LICENSE.third_party`_.
 
 .. _Python: https://www.python.org/
 .. _Google Assistant Library: https://developers.google.com/assistant/sdk/reference/library/python
-.. _Google Assistant SDK: https://developers.google.com/assistant/sdk
-.. _Introduction to the Google Assistant Library: https://developers.google.com/assistant/sdk/guides/library/python/
+.. _Google Assistant SDK for devices: https://developers.google.com/assistant/sdk
+.. _Introduction to the Google Assistant Library: https://developers.google.com/assistant/sdk/guides/library/python
 .. _pip: https://pip.pypa.io/
 .. _GitHub releases page: https://github.com/googlesamples/assistant-sdk-python/releases
-.. _Google API Console Project credentials section: https://console.developers.google.com/apis/credentials
 .. _LICENSE: https://github.com/googlesamples/assistant-sdk-python/tree/master/google-assistant-library/LICENSE
 .. _LICENSE.third_party: https://github.com/googlesamples/assistant-sdk-python/tree/master/google-assistant-library/LICENSE.third_party
 .. _google/assistant/library/LICENSE.third_party: https://github.com/googlesamples/assistant-sdk-python/tree/master/google-assistant-library/google/assistant/library/LICENSE.third_party
--- google-assistant-sdk/googlesamples/assistant/library/hotword.py
+++ google-assistant-sdk/googlesamples/assistant/library/hotword.py
@@ -18,36 +18,33 @@
 from __future__ import print_function
 
 import argparse
-import os.path
 import json
+import os.path
+import pathlib2 as pathlib
 
-import google.auth.transport.requests
 import google.oauth2.credentials
 
 from google.assistant.library import Assistant
 from google.assistant.library.event import EventType
 from google.assistant.library.file_helpers import existing_file
+from google.assistant.library.device_helpers import register_device
+
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
 
 
-DEVICE_API_URL = 'https://embeddedassistant.googleapis.com/v1alpha2'
+WARNING_NOT_REGISTERED = """
+    This device is not registered. This means you will not be able to use
+    Device Actions or see your device in Assistant Settings. In order to
+    register this device follow instructions at:
+
+    https://developers.google.com/assistant/sdk/guides/library/python/embed/register-device
+"""
 
 
-def process_device_actions(event, device_id):
-    if 'inputs' in event.args:
-        for i in event.args['inputs']:
-            if i['intent'] == 'action.devices.EXECUTE':
-                for c in i['payload']['commands']:
-                    for device in c['devices']:
-                        if device['id'] == device_id:
-                            if 'execution' in c:
-                                for e in c['execution']:
-                                    if 'params' in e:
-                                        yield e['command'], e['params']
-                                    else:
-                                        yield e['command'], None
-
-
-def process_event(event, device_id):
+def process_event(event):
     """Pretty prints events.
 
     Prints all events that occur with two spaces between each new
@@ -55,7 +52,6 @@ def process_event(event, device_id):
 
     Args:
         event(event.Event): The current event to process.
-        device_id(str): The device ID of the new instance.
     """
     if event.type == EventType.ON_CONVERSATION_TURN_STARTED:
         print()
@@ -66,44 +62,27 @@ def process_event(event, device_id):
             event.args and not event.args['with_follow_on_turn']):
         print()
     if event.type == EventType.ON_DEVICE_ACTION:
-        for command, params in process_device_actions(event, device_id):
+        for command, params in event.actions:
             print('Do command', command, 'with params', str(params))
-
-
-def register_device(project_id, credentials, device_model_id, device_id):
-    """Register the device if needed.
-
-    Registers a new assistant device if an instance with the given id
-    does not already exists for this model.
-
-    Args:
-       project_id(str): The project ID used to register device instance.
-       credentials(google.oauth2.credentials.Credentials): The Google
-                OAuth2 credentials of the user to associate the device
-                instance with.
-       device_model_id(str): The registered device model ID.
-       device_id(str): The device ID of the new instance.
-    """
-    base_url = '/'.join([DEVICE_API_URL, 'projects', project_id, 'devices'])
-    device_url = '/'.join([base_url, device_id])
-    session = google.auth.transport.requests.AuthorizedSession(credentials)
-    r = session.get(device_url)
-    print(device_url, r.status_code)
-    if r.status_code == 404:
-        print('Registering....')
-        r = session.post(base_url, data=json.dumps({
-            'id': device_id,
-            'model_id': device_model_id,
-            'client_type': 'SDK_LIBRARY'
-        }))
-        if r.status_code != 200:
-            raise Exception('failed to register device: ' + r.text)
-        print('\rDevice registered.')
 
 
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--device-model-id', '--device_model_id', type=str,
+                        metavar='DEVICE_MODEL_ID', required=False,
+                        help='the device model ID registered with Google')
+    parser.add_argument('--project-id', '--project_id', type=str,
+                        metavar='PROJECT_ID', required=False,
+                        help='the project ID used to register this device')
+    parser.add_argument('--device-config', type=str,
+                        metavar='DEVICE_CONFIG_FILE',
+                        default=os.path.join(
+                            os.path.expanduser('~/.config'),
+                            'googlesamples-assistant',
+                            'device_config_library.json'
+                        ),
+                        help='path to store and read device configuration')
     parser.add_argument('--credentials', type=existing_file,
                         metavar='OAUTH2_CREDENTIALS_FILE',
                         default=os.path.join(
@@ -111,40 +90,59 @@ def main():
                             'google-oauthlib-tool',
                             'credentials.json'
                         ),
-                        help='Path to store and read OAuth2 credentials')
-    parser.add_argument('--device_model_id', type=str,
-                        metavar='DEVICE_MODEL_ID', required=True,
-                        help='The device model ID registered with Google')
-    parser.add_argument(
-        '--project_id',
-        type=str,
-        metavar='PROJECT_ID',
-        required=False,
-        help='The project ID used to register device instances.')
-    parser.add_argument(
-        '-v',
-        '--version',
-        action='version',
-        version='%(prog)s ' +
-        Assistant.__version_str__())
+                        help='path to store and read OAuth2 credentials')
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s ' + Assistant.__version_str__())
 
     args = parser.parse_args()
     with open(args.credentials, 'r') as f:
         credentials = google.oauth2.credentials.Credentials(token=None,
                                                             **json.load(f))
 
-    with Assistant(credentials, args.device_model_id) as assistant:
+    device_model_id = None
+    last_device_id = None
+    try:
+        with open(args.device_config) as f:
+            device_config = json.load(f)
+            device_model_id = device_config['model_id']
+            last_device_id = device_config.get('last_device_id', None)
+    except FileNotFoundError:
+        pass
+
+    if not args.device_model_id and not device_model_id:
+        raise Exception('Missing --device-model-id option')
+
+    # Re-register if "device_model_id" is given by the user and it differs
+    # from what we previously registered with.
+    should_register = (
+        args.device_model_id and args.device_model_id != device_model_id)
+
+    device_model_id = args.device_model_id or device_model_id
+
+    with Assistant(credentials, device_model_id) as assistant:
         events = assistant.start()
 
-        print('device_model_id:', args.device_model_id + '\n' +
-              'device_id:', assistant.device_id + '\n')
+        device_id = assistant.device_id
+        print('device_model_id:', device_model_id)
+        print('device_id:', device_id + '\n')
 
-        if args.project_id:
-            register_device(args.project_id, credentials,
-                            args.device_model_id, assistant.device_id)
+        # Re-register if "device_id" is different from the last "device_id":
+        if should_register or (device_id != last_device_id):
+            if args.project_id:
+                register_device(args.project_id, credentials,
+                                device_model_id, device_id)
+                pathlib.Path(os.path.dirname(args.device_config)).mkdir(
+                    exist_ok=True)
+                with open(args.device_config, 'w') as f:
+                    json.dump({
+                        'last_device_id': device_id,
+                        'model_id': device_model_id,
+                    }, f)
+            else:
+                print(WARNING_NOT_REGISTERED)
 
         for event in events:
-            process_event(event, assistant.device_id)
+            process_event(event)
 
 
 if __name__ == '__main__':
